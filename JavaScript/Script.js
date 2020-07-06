3fa50ab23ab611da3f1f9126a8ee05ac51b78ee6
JuliaBordo <65512176+JuliaBordo@users.noreply.github.com>
2020-07-06 23:13:16 +0300

    Add files via upload

---
+++ JavaScript/Script.js
@@ -0,0 +1,36 @@
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+  }
+
+function openNavFood() {
+  document.getElementById("mySidenavMenu").style.width = "100%";
+}
+
+function closeNavFood() {
+  document.getElementById("mySidenavMenu").style.width = "0";
+}
+
+function openNavDrinks() {
+  document.getElementById("mySidenavMenu2").style.width = "100%";
+}
+
+function closeNavDrinks() {
+  document.getElementById("mySidenavMenu2").style.width = "0";
+}
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNavMoscow() {
+  document.getElementById("mySidenavMoscow").style.width = "250px";
+  document.getElementById("mainMoscow").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNavMoscow() {
+  document.getElementById("mySidenavMoscow").style.width = "0";
+  document.getElementById("mainMoscow").style.marginLeft = "0";
+}
+
